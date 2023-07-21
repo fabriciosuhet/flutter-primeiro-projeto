b10b44cd513fd63ee035cb4b8f9415cd0dd91642
Fabrício Suhet <fabriciosuhet@hotmail.com>
2023-07-21 14:02:14 -0300

    device preview e adicionando ele no pubspac

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_primeiro_projeto/pages/container/containerpage.dart';
 import 'package:flutter_primeiro_projeto/pages/home/homepage.dart';
@@ -5,7 +7,12 @@ import 'package:flutter_primeiro_projeto/pages/media_query/media_query_page.dart
 import 'package:flutter_primeiro_projeto/pages/rows_columns/rows_columns_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: false,
+      builder: (_) => const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -20,6 +27,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       routes: {
         '/': (_) => const HomePage(),
         '/container': (_) => const ContainerPage(),
